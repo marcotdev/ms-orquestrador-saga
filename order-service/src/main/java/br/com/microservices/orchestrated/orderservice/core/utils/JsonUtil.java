badc22f4052c3b03cc1715425e93498668469a41
Marco Antonio Teixeira <129874530+marcotdev@users.noreply.github.com>
2023-11-04 11:49:20 -0300

    primeiro commit
    Arquivos de microservices

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/utils/JsonUtil.java
@@ -0,0 +1,33 @@
+package br.com.microservices.orchestrated.orderservice.core.utils;
+
+
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+
+@Component
+@RequiredArgsConstructor
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception ex) {
+            return "";
+        }
+    }
+
+    public Event toEvent(String json) {
+        try {
+            return objectMapper.readValue(json, Event.class);
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+}
