70e5bf3d2266bf34c299fdb60031e43ab4266280
marcotdev <marcotdev@gmail.com>
2023-11-12 11:00:32 -0300

    completando microserviços inventory-service, productValidation-service e payment-service

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/consumer/PaymentConsumer.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/consumer/PaymentConsumer.java
@@ -1,19 +1,31 @@
 package br.com.microservices.orchestrated.paymentservice.core.consumer;
 
 
+<<<<<<< HEAD
 import br.com.microservices.orchestrated.paymentservice.core.service.PaymentService;
 import br.com.microservices.orchestrated.paymentservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
+=======
+import br.com.microservices.orchestrated.paymentservice.core.utils.JsonUtil;
+import lombok.RequiredArgsConstructor;
+>>>>>>> 3efde44 (add consumer aos microserviços)
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+<<<<<<< HEAD
 @AllArgsConstructor
 public class PaymentConsumer {
 
     private final PaymentService paymentService;
+=======
+@RequiredArgsConstructor
+public class PaymentConsumer {
+
+   // private final PaymentService paymentService;
+>>>>>>> 3efde44 (add consumer aos microserviços)
     private final JsonUtil jsonUtil;
 
     @KafkaListener(
@@ -23,8 +35,13 @@ public class PaymentConsumer {
     public void consumeSuccessEvent(String payload) {
         log.info("Receiving success event {} from payment-success topic", payload);
         var event = jsonUtil.toEvent(payload);
+<<<<<<< HEAD
        paymentService.realizePayment(event);
 
+=======
+       // paymentService.realizePayment(event);
+        log.info(event.toString());
+>>>>>>> 3efde44 (add consumer aos microserviços)
     }
 
     @KafkaListener(
@@ -34,6 +51,11 @@ public class PaymentConsumer {
     public void consumeFailEvent(String payload) {
         log.info("Receiving rollback event {} from payment-fail topic", payload);
         var event = jsonUtil.toEvent(payload);
+<<<<<<< HEAD
         paymentService.realizeRefund(event);
+=======
+        //paymentService.realizeRefund(event);
+        log.info(event.toString());
+>>>>>>> 3efde44 (add consumer aos microserviços)
     }
 }
--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/consumer/ProductValidationConsumer.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/consumer/ProductValidationConsumer.java
@@ -1,7 +1,10 @@
 package br.com.microservices.orchestrated.productvalidationservice.core.consumer;
 
 
+<<<<<<< HEAD
 import br.com.microservices.orchestrated.productvalidationservice.core.service.ProductValidationService;
+=======
+>>>>>>> 3efde44 (add consumer aos microserviços)
 import br.com.microservices.orchestrated.productvalidationservice.core.utils.JsonUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +16,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ProductValidationConsumer {
 
+<<<<<<< HEAD
     private final ProductValidationService productValidationService;
+=======
+    //private final ProductValidationService productValidationService;
+>>>>>>> 3efde44 (add consumer aos microserviços)
     private final JsonUtil jsonUtil;
 
     @KafkaListener(
@@ -23,8 +30,13 @@ public class ProductValidationConsumer {
     public void consumeSuccessEvent(String payload) {
         log.info("Receiving success event {} from product-validation-success topic", payload);
         var event = jsonUtil.toEvent(payload);
+<<<<<<< HEAD
         productValidationService.validateExistingProducts(event);
 
+=======
+        //productValidationService.validateExistingProducts(event);
+        log.info(event.toString());
+>>>>>>> 3efde44 (add consumer aos microserviços)
     }
 
     @KafkaListener(
@@ -34,6 +46,11 @@ public class ProductValidationConsumer {
     public void consumeFailEvent(String payload) {
         log.info("Receiving rollback event {} from product-validation-fail topic", payload);
         var event = jsonUtil.toEvent(payload);
+<<<<<<< HEAD
         productValidationService.rollbackEvent(event);
+=======
+        //productValidationService.rollbackEvent(event);
+        log.info(event.toString());
+>>>>>>> 3efde44 (add consumer aos microserviços)
     }
 }
